92be23df67f1b9f303dfd400377469c4b16d70be
Tan Nguyen <dtphatkh@gmail.com>
2024-01-12 20:28:07 +0700

    update UI

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const BASE_URL = 'https://recipehub.herokuapp.com'
-// const BASE_URL = 'https://recipehub.up.railway.app'
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://recipehub.up.railway.app'
+// const BASE_URL = 'http://localhost:8080'
 export default axios.create({
   baseURL: BASE_URL,
   withCredentials: true
